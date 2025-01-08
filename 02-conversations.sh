eb676c58a2fcbbecca21d8f317bbcd83db4df9ad
apkanagesh <apkanagesh@gmail.com>
2025-01-08 18:37:38 +0000

    I added conversations script

---
+++ 02-conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh::Hi suresh,how are you?"
+echo "Suresh::"Hello ramesh. Iam fine. how are you doing?"
+echo "Ramesh:: Iam doing good suresh.what's going on?"
+echo "suresh :: I started learning shell scripts Ramesh"
