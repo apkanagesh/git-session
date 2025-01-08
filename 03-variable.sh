4b9da2508ce9097d66460c1974759423b3918681
apkanagesh <apkanagesh@gmail.com>
2025-01-08 19:06:49 +0000

    03-variable.sh

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1::Hi ${PERSON2},how are you?"
+echo "${PERSON2}::"Hello $PERSON1. Iam fine. how are you doing?"
+echo "$PERSON1:: Iam doing good ${PERSON2}.what's going on?"
+echo "${PERSON2}:: I started learning shell script $PERSON1"
