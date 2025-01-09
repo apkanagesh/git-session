4e47bf2afcd1b6130797c560c1752515dfe27ba9
apkanagesh <apkanagesh@gmail.com>
2025-01-09 11:56:15 +0000

    09-special-vars.sh

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -2,3 +2,7 @@
 
 echo "All variables passed to the scrtipts:$@"
 echo "Number of variable passed: $#"
+echo "script name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $home"
+ech "PID of the script executing now: $$"
