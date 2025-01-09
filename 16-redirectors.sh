5af6a1e495dc3dc1b6c095562762f20f25d1a7b7
apkanagesh <apkanagesh@gmail.com>
2025-01-09 18:01:33 +0000

    16-redirectors.sh

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$ (echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
