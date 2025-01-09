8ffc2be017aedb8c333819a36a32544bd2f164d6
apkanagesh <apkanagesh@gmail.com>
2025-01-09 14:22:46 +0000

    11-installation.sh

--- 11-installation.sh
+++ 11-installation.sh
@@ -15,6 +15,30 @@ if [ $? -ne 0 ]
 then
     echo "Git is not installed, going to install it.."
     dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "git installation is not success...check it"
+        exit 1        
 else
+    echo "git installation is success"
+  fi
+else    
     echo "Git is already installed, nothing to do.."
-fi    
+fi   
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed...going to install"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+       echo "MYSQL installation is failure..please check"
+       exit1
+    else
+       echo  "MYSQL installation is success"
+    fi
+else
+       echo "MYSQL is already installed..nothing to do"
+  fi        
